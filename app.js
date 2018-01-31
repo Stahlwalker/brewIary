41d69560f7389521ccd638f6d31edfcf3c48c89a
Luke Stahl <Stahl1109@gmail.com>
2018-01-31 12:36:12 -0600

    updated user

--- app.js
+++ app.js
@@ -6,18 +6,14 @@ jQuery.ajaxPrefilter(function (options) {
 
 $("#run-search").on("click", function () {
     event.preventDefault();
-
     $("#beer-body").empty();
     clearMarkers();
     deleteMarkers();
     beerList = [];
     latArr = [];
-
     var beer = $("#beer-style").val().trim();
-
     var queryURL = "https://api.brewerydb.com/v2/search?q=" + beer +
         "&withBreweries=Y&type=beer&key=3be3e987ffecfe82a589838b384c138f&format=json";
-
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -25,9 +21,7 @@ $("#run-search").on("click", function () {
         console.log(response);
         var beers = response.data;
         console.log(beers[0].breweries[0].name);
-
         var count = 0;
-
         for (var i = 0; i < beers.length; i++) {
             var glass;
             if (beers[i].breweries[0].hasOwnProperty('locations')) {
@@ -39,26 +33,19 @@ $("#run-search").on("click", function () {
                 $("#beer-table > tbody").append("<tr id='" + count + "'><td>" + beers[i].name + "</td><td>" + beers[i].breweries[0].name + "</td><td>" +
                     beers[i].abv + "%</td><td>" + beers[i].style.name +
                     "</td><td>" + glass + "</td></tr>");
-
                 beerList.push(beers[i]);
                 createMarker(beers[i]);
                 count++;
-
-
             }
         }
     });
     $("#beer-style").val("");
     initMap();
-
 });
-
 //Code for the Google Map is below, used brewery location information from
 //the BreweryDB
-
 var map;
 var markers = [];
-
 var infowindow;
 var service;
 var geocoder;
@@ -71,32 +58,25 @@ function createMarker(beer) {
     var lng = beer.breweries[0].locations[0].longitude;
     var place = new google.maps.LatLng(lat, lng);
     latArr.push(place);
-
     var streetAddress = beer.breweries[0].locations[0].streetAddress;
     var website = beer.breweries[0].locations[0].website;
     var name = beer.breweries[0].name;
     var city = beer.breweries[0].locations[0].locality;
     var state = beer.breweries[0].locations[0].region;
-
     var formatted_address = streetAddress + ", " + city + ", " + state;
-
     if (typeof website == "undefined") {
         website = "";
     }
-
     var marker = new google.maps.Marker({
         map: map,
         position: place
     });
-
     markers.push(marker);
-
     google.maps.event.addListener(marker, 'click', function () {
         infowindow.setContent('<div><strong>' + name + '</strong><br>' +
             formatted_address + '<br>' + "<a href='" + website + "' >" + website + "</a>" + '</div>');
         infowindow.open(map, this);
     });
-
 }
 
 function callback(place, status) {
@@ -125,26 +105,20 @@ function deleteMarkers() {
 }
 
 function initMap() {
-
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 3,
         center: new google.maps.LatLng(37.09024, -95.712891),
         mapTypeId: 'terrain'
     });
     infowindow = new google.maps.InfoWindow();
-
     geocoder = new google.maps.Geocoder;
-
 };
-
 //Zoom in on clicked beer in results table
 $(document).on("mouseover", "tr", function () {
-
     var id = $(this).attr('id');
     var place = latArr[id];
     map.setCenter(place);
     map.setZoom(10);
-
 });
 
 $(document).on("click", "tr", function () {
@@ -152,23 +126,19 @@ $(document).on("click", "tr", function () {
     var id = $(this).attr('id');
     var beer = beerList[id];
     console.log(beer.name);
-
     var beerName = $("<h2>");
     beerName.text(beer.name);
     $('#beer-info').append(beerName);
-
     if (typeof beer.labels != 'undefined') {
         var beerImg = $("<img>");
         beerImg.attr('src', beer.labels.medium);
         $('#beer-info').append(beerImg);
     }
-
     var beerDsc = $("<p>");
     beerDsc.text(beer.description);
     $('#beer-info').append(beerDsc);
-
+    
     document.getElementById('beer-popup').style.display = 'block';
-
 });
 
 $(document).on("click", "#beer-close", function () {
@@ -177,99 +147,137 @@ $(document).on("click", "#beer-close", function () {
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyC_B1Ocq23JwTvXP4cSlaCEYHe5TqR1220",
-    authDomain: "personal-firebase-8c99a.firebaseapp.com",
-    databaseURL: "https://personal-firebase-8c99a.firebaseio.com",
-    projectId: "personal-firebase-8c99a",
-    storageBucket: "",
-    messagingSenderId: "71144171999"
+    apiKey: "AIzaSyAm_nwrEHI-HAzrUa-YiRCMbcuoA24x7vA",
+    authDomain: "breweryproject-223c8.firebaseapp.com",
+    databaseURL: "https://breweryproject-223c8.firebaseio.com",
+    projectId: "breweryproject-223c8",
+    storageBucket: "breweryproject-223c8.appspot.com",
+    messagingSenderId: "675321245147"
 };
+
 firebase.initializeApp(config);
+
+var uid;
 
 const txtEmail = document.getElementById('txtEmail');
 const txtPassword = document.getElementById('txtPassword');
 const btnLogin = document.getElementById('btnLogin');
 const btnSignUp = document.getElementById('btnSignUp');
 const btnLogout = document.getElementById('btnLogout');
-
 btnLogin.addEventListener('click', e => {
     const email = txtEmail.value;
     const pass = txtPassword.value;
     const auth = firebase.auth();
-
     const promise = auth.signInWithEmailAndPassword(email, pass);
     promise.catch(e => console.log(e.message));
 });
-
 btnSignUp.addEventListener('click', e => {
     const email = txtEmail.value;
     const pass = txtPassword.value;
     const auth = firebase.auth();
-
     const promise = auth.createUserWithEmailAndPassword(email, pass);
     promise.catch(e => console.log(e.message));
 });
-
 btnLogout.addEventListener('click', e => {
+    var uid = "";
     firebase.auth().signOut();
 });
 
-firebase.auth().onAuthStateChanged(firebaseUser => {
-    if (firebaseUser) {
-        console.log(firebaseUser);
-        btnLogout.classList.remove('hide');
-    } else {
-        console.log('not logged in');
-        btnLogout.classList.add('hide');
-    }
-});
+
+
+
 
 $("#btnLogin").on("click", function (event) {
     $("#txtEmail").val("");
     $("#txtPassword").val("");
-
 });
-
 $("#btnSignUp").on("click", function (event) {
     $("#txtEmail").val("");
     $("#txtPassword").val("");
-
 });
-
 // Create a variable to reference the database.
 var database = firebase.database();
-
-
 // Whenever a user clicks the click `
+
+
+
+firebase.auth().onAuthStateChanged(firebaseUser => {
+    if (firebaseUser) {
+        console.log(firebaseUser.uid);
+        console.log(firebaseUser);
+        uid = firebaseUser.uid;
+        btnLogout.classList.remove('hide');
+         database.ref('users/' + uid).off("child_added");
+
+        //var usersRef = database.ref.child("users");
+        
+        //console.log(eventID);    
+
+         // var where = "Where'd you find it?";
+         // var what = "What'd you think?";
+         // var name = "Name";
+
+        // var html = '<table class="table" id="user-table">' + 
+        //    '<thead>' + '<tr>'
+        //        '<th>"'+ name +'"</th>' +
+        //        '<th>"'+ where + '"</th>' +
+        //        '<th>"' + what + '"</th>' +
+        //    '</tr></thead></table>';
+        // $("#beer-section2").append(html);  
+        $("#beer-table2 > tbody").empty();
+        database.ref('users/' + uid).on("child_added", function(childSnapshot){  
+            
+
+        $("#beer-table2 > tbody").append("<tr><td>" + childSnapshot.val().beerName + "</td><td>" + 
+            childSnapshot.val().location + "</td><td>" + childSnapshot.val().thoughts + "</td></tr>");
+
+        });
+        
+    } else {
+        console.log('not logged in');
+        uid = "";
+        database.ref('public/' + uid).off("child_added");
+        $("#beer-table2 > tbody").empty();
+
+        database.ref('public/').on("child_added", function (childSnapshot) {
+    // Log everything that's coming out of snapshot
+            console.log(childSnapshot.val());
+             
+            $("#beer-table2 > tbody").append("<tr><td>" + childSnapshot.val().beerName + "</td><td>" + 
+                childSnapshot.val().location + "</td><td>" + childSnapshot.val().thoughts + "</td></tr>");
+            }, function (errorObject) {});
+            $("#add-beer").on("click", function (event) {
+            $("#beer-name").val("");
+            $("#location").val("");
+            $("#your-thoughts").val("");
+        });
+        btnLogout.classList.add('hide');
+    }
+});
+
 $("#add-beer").on("click", function (event) {
     event.preventDefault();
-
     // Get the input values
-
     var beerName = $("#beer-name").val().trim();
     var location = $("#location").val().trim();
     var thoughts = $("#your-thoughts").val().trim();
+    //Get the user ID
+    //var newRef = database.ref('users/' + uid).push();
+    //eventID = newRef.key;
+    //console.log(eventID);
+    if(uid !== ""){
+        database.ref('users/' + uid).push({
+            beerName: beerName,
+            location: location,
+            thoughts: thoughts,
+        });
+    }
+    else if(uid === ""){
+        database.ref('public/' + uid).push({
+            beerName: beerName,
+            location: location,
+            thoughts: thoughts,
+        });
 
-
-    database.ref().push({
-        beerName: beerName,
-        location: location,
-        thoughts: thoughts,
-    });
-});
-
-database.ref().on("child_added", function (childSnapshot) {
-
-    // Log everything that's coming out of snapshot
-    console.log(childSnapshot.val());
-
-    $("#beer-table2 > tbody").append("<tr><td>" + childSnapshot.val().beerName + "</td><td>" + childSnapshot.val().location + "</td><td>" + childSnapshot.val().thoughts + "</td></tr>");
-
-}, function (errorObject) {});
-
-$("#add-beer").on("click", function (event) {
-    $("#beer-name").val("");
-    $("#location").val("");
-    $("#your-thoughts").val("");
-
+    }
 });
